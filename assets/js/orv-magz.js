26318cba9ce82d2816835b86447245b9afc00572
DESKTOP-OO15DKO\fani <orvynproject@gmail.com>
2016-10-08 00:44:58 +0700

    animation

---
+++ assets/js/orv-magz.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	$("#top-slider").owlCarousel({
+		navigation : true,
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem:true,
+		autoPlay: 2000,
+		stopOnHover : true
+	});
+	$("#news-slider").owlCarousel({
+		navigation : true,
+		dots: false,
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem:true,
+		autoPlay: 2000,
+		stopOnHover : true
+	});
+	$("#news-gallery").owlCarousel({
+		autoPlay: 2000,
+		items : 4,
+		itemsDesktop : [1199,3],
+		itemsDesktopSmall : [979,3]
+	});
+	$("#image-gallery").owlCarousel({
+		autoPlay: 2000,
+		items : 6,
+		itemsDesktop : [1199,3],
+		itemsDesktopSmall : [979,3]
+	});
+	$("#powered-gallery").owlCarousel({
+		autoPlay: 2000,
+		items : 6,
+		itemsDesktop : [1199,3],
+		itemsDesktopSmall : [979,3]
+	});
+});
+
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 400) {
+		$('.gotop').fadeIn(200);
+	}else{
+		$('.gotop').fadeOut(200);
+	}
+});
+$('.gotop').click(function(event) {
+	event.preventDefault();
+	$('html, body').animate({scrollTop: 0}, 1000);
+});
